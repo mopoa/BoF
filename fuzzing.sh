8fad928fe85cb9bcb38894b846d494912336e9bd
mamzi <mr.pourbaba@gmail.com>
2024-05-23 09:09:17 +0330

    init

---
+++ fuzzing.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for (( itr=1; i<= 9999999; itr++));
+do
+    echo "round $itr";
+# Enter the fuzzing code here
+    python -c "print('A'* $itr )" | /home/mamzi/Desktop/vulnApp.out ;
+    if [ $? -ne 0 ]
+    then
+        echo "$itr character is needed to crash";
+	break
+    fi
+    pkill vulnApp;
+
+
+done
+
