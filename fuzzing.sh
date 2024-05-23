9cc5c912341332578e8c700bf495d6309e797292
Muhammad R. Pourbaba <41018831+mopoa@users.noreply.github.com>
2024-05-23 23:29:58 +0330

    Update fuzzing.sh

--- fuzzing.sh
+++ fuzzing.sh
@@ -1,18 +1,1 @@
-#!/bin/bash
-
-for (( itr=1; i<= 9999999; itr++));
-do
-    echo "round $itr";
-# Enter the fuzzing code here
-    python -c "print('A'* $itr )" | /home/mamzi/Desktop/vulnApp.out ;
-    if [ $? -ne 0 ]
-    then
-        echo "$itr character is needed to crash";
-	break
-    fi
-# Remember to kill the app
-    pkill vulnApp;
-
-
-done
 
